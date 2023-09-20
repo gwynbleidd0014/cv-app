d7524f5b630d0dbe29660784aca1b543f33e37b6
Ucha <mr.nemo0014@gmail.com>
2023-09-20 13:08:15 +0400

    new begining

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/button.css";
-
-export default function Button({ className, handleClick, text }) {
-  return (
-    <button className={className} onClick={handleClick}>
-      {text}
-    </button>
-  );
-}
